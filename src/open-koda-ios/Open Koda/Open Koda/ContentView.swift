fa3338181fc97bfde7c3f3ccb03e665ba32d40d8
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-12-17 22:16:24 -0300

    feat: initial commit

---
+++ src/open-koda-ios/Open Koda/Open Koda/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Open Koda
+//
+//  Created by Gabriel Araújo on 17/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ src/open-koda-ios/Open Koda/Open Koda/Open_KodaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Open_KodaApp.swift
+//  Open Koda
+//
+//  Created by Gabriel Araújo on 17/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Open_KodaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
